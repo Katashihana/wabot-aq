d2cf372898c6f4da653d409f58a2236da0362e18
Katashihana <89012832+Katashihana@users.noreply.github.com>
2021-09-28 00:50:17 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '6281334177340', 'Nurutomo', m)
+  // F this.sendContact(m.chat, '6289626029135', 'katashi', m)
+  this.sendContact(m.chat, '6289626029135', 'Katashi', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
